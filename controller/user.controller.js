9bb4a16eb9a97fb525a2be0062055b708cd83b33
YogeshSen24 <yose0026@gmail.com>
2024-04-27 21:06:21 +0530

    Update user controller and routes; add getAllFriends, searchUser functions
    The user controller has been updated to include getAllFriends and searchUser functions. The getAllFriendRequests function has been modified to include a populate method for

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,87 +1,123 @@
+import User from "../models/user.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import myError from "../utils/customErrors.js";
+import Response from "../utils/customResponses.js";
+import bcrypt from "bcryptjs";
 
-import User from "../models/user.model.js"
-import asyncHandler from "../utils/asyncHandler.js" 
-import myError from "../utils/customErrors.js"
-import Response from "../utils/customResponses.js"
-import bcrypt from "bcryptjs"
+const getSelf = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
 
+  const user = await User.findById({ _id });
+  if (!user) throw new myError("User not found", 404);
 
-const getSelf = asyncHandler(
-    async(req , res)=>{
-        const {_id} = req.user
+  Response(res, user, 200, "User data fetched successfully");
+});
+const getUser = asyncHandler(async (req, res) => {
+  const id = req.params.userId;
+  const user = await User.findById(id).select("-password");
+  if (!user) throw new myError("User not found", 404);
 
-        const user = await User.findById({_id})
-        if(!user) throw new myError("User not found" , 404)
+  Response(res, user, 200, "User data fetched successfully");
+});
+const resetPassword = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { oldPassword, newPassword } = req.body;
+  let user = await User.findById(_id);
+  if (!user) throw new myError("User not found", 404);
+  const isValid = await bcrypt.compare(oldPassword, user.password);
+  if (!isValid) throw new myError("Invalid password", 400);
+  user.password = newPassword;
+  const isSaved = await user.save();
+  if (!isSaved)
+    throw new myError(
+      "SOmething went wrong while saving the new password",
+      500
+    );
 
-        Response(res , user , 200 , "User data fetched successfully")
-    }
-)
-const getUser = asyncHandler(
-    async(req , res)=>{
-        const id = req.params.userId
-        const user = await User.findById(id).select("-password")
-        if(!user) throw new myError("User not found" , 404)
+  Response(res, user, 200, "Password changed successfully");
+});
+const updateUser = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { username, status, bio } = req.body;
+  let user = await User.findById(_id);
+  const profileImage = req.fileUrls ? req.fileUrls[0] : user.profileImage;
+  if (!user) throw new myError("User not found", 404);
 
-        Response(res , user , 200 , "User data fetched successfully")
-    }
-)
-const resetPassword = asyncHandler(
-    async(req , res)=>{
-        const {_id} = req.user
-        const {oldPassword , newPassword} = req.body
-        let user = await User.findById(_id)
-        if(!user) throw new myError("User not found" , 404)
-        const isValid = await bcrypt.compare(oldPassword , user.password )
-        if(!isValid) throw new myError("Invalid password" , 400)
-        user.password = newPassword
-        const isSaved = await user.save()
-        if(!isSaved) throw new myError("SOmething went wrong while saving the new password" , 500)
+  user.username = username || user.username;
+  user.profileImage = profileImage || user.profileImage;
+  user.bio = bio || user.bio;
+  user.status = status || user.status;
 
-        Response(res , user , 200 , "Password changed successfully")
-    }
-)
-const updateUser = asyncHandler(
-    async(req , res)=>{
-        const {_id} = req.user
-        const {username , status , bio} = req.body
-        let user = await User.findById(_id)
-        const profileImage = req.fileUrls? req.fileUrls[0] : user.profileImage
-        if(!user) throw new myError("User not found" , 404)
+  const updatedUser = await user.save();
+  if (!updatedUser) {
+    throw new myError(
+      "Something went wrong while saving the new password",
+      500
+    );
+  }
 
-        user.username = username || user.username;
-        user.profileImage = profileImage || user.profileImage ;
-        user.bio = bio || user.bio;
-        user.status = status || user.status;
+  Response(res, user, 200, "Data updated successfully");
+});
 
-        const updatedUser = await user.save();
-        if (!updatedUser) {
-            throw new myError("Something went wrong while saving the new password", 500);
-        }
-
-
-        Response(res , user , 200 , "Data updated successfully")
-    }
-)
-
-const getAllFriendRequests = asyncHandler( async(req ,res)=>{
-    const {_id} = req.user
-    const me = await User.findById(_id).select("friendRequests").populate({
-        path: "friendRequests",
-        select: "_id type",
+const getAllFriendRequests = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const me = await User.findById(_id)
+    .select("friendRequests")
+    .populate({
+      path: "friendRequests",
+      select: "_id type",
+      populate: {
+        path: "requester",
+        select: "_id username email profileImage",
         populate: {
-          path: "requester",
-          select: "_id username email profileImage",
-          populate: {
-            path: "friends",
-            select: "_id profileImage"
-          }
+          path: "friends",
+          select: "_id profileImage",
+        },
+      },
+    });
+
+  if (!me) throw new myError("User not found", 404);
+  if (me.friendRequests.length === 0)
+    throw new myError("No friend requests found", 404);
+
+  Response(res, me, 200, "Friend requests fetched successfully");
+});
+
+const getAllFriends = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const user = await User.findById(_id).select("+friends").populate("friends");
+  if (!user) throw new myError("No friends found", 404);
+  Response(res, user.friends, 200, "Friends fetched successfully");
+});
+const searchUser = asyncHandler(async (req, res) => {
+  const { data } = req.params;
+  const {_id : currentUserId} = req.user
+
+  // Find users matching the username
+  const matchedUsers = await User.find({
+    $and: [ // Add an additional condition to exclude the current user
+        { _id: { $ne: currentUserId } }, // Exclude documents with the current user's ID
+        {
+          $or: [
+            { username: { $regex: `${data}.*`, $options: "i" } },
+            { email: { $regex: `${data}.*`, $options: "i" } },
+          ],
         }
-      });
-      
-    if(!me) throw new myError("User not found" , 404)
-    if(me.friendRequests.length === 0) throw new myError("No friend requests found" , 404)
+      ]
+  })
+    .select("_id username email profileImage bio")
+    .populate({ path: "friends" , select : "username _id email profileImage"}); // Select fields you want to send in the response
 
-    Response(res , me , 200 , "Friend requests fetched successfully")
-})
+  // Send the response
+  Response(res, matchedUsers, 200, "Users found successfully");
+});
 
-export {getUser , getSelf , resetPassword , updateUser , getAllFriendRequests }
+export {
+  getUser,
+  getSelf,
+  resetPassword,
+  updateUser,
+  getAllFriendRequests,
+  getAllFriends,
+  searchUser
+};
--- route/user.route.js
+++ route/user.route.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import { getUser, resetPassword , getSelf , updateUser, getAllFriendRequests } from '../controller/user.controller.js';
+import { getUser, resetPassword , getSelf , updateUser, getAllFriendRequests, getAllFriends, searchUser } from '../controller/user.controller.js';
 import { authenticateUser } from '../middlewares/auth.middleware.js';
 import multerUpload from '../middlewares/multer.middleware.js';
 import uploadToCloudinary from '../middlewares/cloudinary.middleware.js';
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.get("/:userId",  getUser)
 router.get("/", authenticateUser , getSelf)
+router.get("/search/:data", authenticateUser , searchUser)
+router.get("/friends/get", authenticateUser , getAllFriends)
 router.get("/requests/get", authenticateUser , getAllFriendRequests)
 router.put("/resetPassword", authenticateUser , resetPassword)
 router.put("/update", authenticateUser ,multerUpload.single("avatar") , uploadToCloudinary, updateUser)
