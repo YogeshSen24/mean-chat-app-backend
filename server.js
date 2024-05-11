11194c187139a38b56d172f373c88bb319bf83ed
YogeshSen24 <yose0026@gmail.com>
2024-05-11 09:36:26 +0530

    Update CORS origin to include "http://localhost:5173"
    The code diff shows the modification of the `cors` property in the Server object created with the `socket.io` library. The `origin` value

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://owl-chat.netlify.app",
+    origin: ["https://owl-chat.netlify.app" , "http://localhost:5173"],
     methods: ["GET", "POST" , "PUT" , "DELETE"]
   },
 });
